58f92849a19882cb030a94be22dfe538dba4912a
Carleton Maybell <cmaybell@aroptions.com>
2017-05-19 14:11:38 -0500

    Moves heroes model to a separate service. Implements a promise in app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -77,15 +65,29 @@ const HEROES: Hero[] = [
       </li>
     </ul>
     <hero-detail [hero]="selectedHero"></hero-detail>
-  `
+  `,
+  providers: [HeroService]
 })
 
-export class AppComponent  { 
+export class AppComponent implements OnInit  { 
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes(); 
+  }
+
   title = 'Tour of Heroes';
   //we need to do this to "expose the heroes for binding"
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
   onSelect(hero: Hero):void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then( heroes =>
+      this.heroes = heroes
+    );
   }
 }
