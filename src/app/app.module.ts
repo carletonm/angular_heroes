d2b5761497883d96e3af3cdf47ebad146cde16f9
Carleton Maybell <cmaybell@aroptions.com>
2017-05-22 14:07:14 -0500

    Completes the Tour of Heroes application. Adds mock API support for get, post, put, and delete requests.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,35 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-//import { RouterModule }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
+
 import { AppRoutingModule }   from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [ 
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   providers: [
     HeroService
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -29,12 +29,31 @@ export class HeroesComponent implements OnInit  {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroesSlowly().then( heroes =>
+    this.heroService.getHeroes().then( heroes =>
       this.heroes = heroes
     );
   }
 
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => { //use curly braces for multiline after =>
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        });
   }
 }
